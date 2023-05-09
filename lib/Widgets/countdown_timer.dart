091f612d3729bb6dd0b37c40f68a7bfd4c79566d
KrishnaPalde <paldekrishna4325@gmail.com>
2023-05-09 14:52:29 +0530

    Some More Changes Done

--- lib/Widgets/countdown_timer.dart
+++ lib/Widgets/countdown_timer.dart
@@ -3,9 +3,11 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_countdown_timer/flutter_countdown_timer.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:gpauth_004/Models/gpauth_algorithms.dart';
 
 class CustomCountDownTimer extends StatefulWidget {
-  const CustomCountDownTimer({super.key});
+  const CustomCountDownTimer({super.key, required this.onTimerEndsCall});
+  final VoidCallback onTimerEndsCall;
 
   @override
   State<CustomCountDownTimer> createState() => _CustomCountDownTimerState();
@@ -18,7 +20,10 @@ class _CustomCountDownTimerState extends State<CustomCountDownTimer> {
       endTime: DateTime.now().millisecondsSinceEpoch + 1000 * 30,
       onEnd: () {
         print("Completed");
-        setState(() {});
+        setState(() {
+          GPAuthAlgorithms.getRandomImageSetExceptCurrentSet();
+          widget.onTimerEndsCall();
+        });
       },
       widgetBuilder: (_, time) {
         if (time == null) {
